c3fae1aa76df08a6f66e3101ae83b732f6e3000d
moraisLuismNet <morais.luism.net@gmail.com>
2025-02-17 09:53:05 +0100

    17.02.2025_ScheduleTask+error

--- Store/Program.cs
+++ Store/Program.cs
@@ -1,20 +1,29 @@
 using Microsoft.EntityFrameworkCore;
+using Store.Middlewares;
+using Store.Filters;
 using Store.Models;
 using Store.Services;
 using System.Text.Json.Serialization;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
-string connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING") ??
-                            builder.Configuration.GetConnectionString("DefaultConnection");
+// This option is to avoid circular references when using include in controllers
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add(typeof(ExceptionFilter));
+
+}).AddJsonOptions(options => options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 
 builder.Services.AddDbContext<StoreContext>(options =>
 {
     options.UseSqlServer(connectionString);
+    // Disable tracking
     options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
 }
 );
@@ -27,6 +36,15 @@ builder.Services.AddTransient<ActionsService>();
 builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddTransient<IFileManagerService, FileManagerService>();
+builder.Services.AddHostedService<ScheduledTaskService>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
@@ -37,13 +55,17 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-// Middleware to access static files in the wwwroot folder
-app.UseStaticFiles();
-
 app.UseHttpsRedirection();
+
+app.UseCors();
 
 app.UseAuthorization();
 
+app.UseMiddleware<RegisterAndControlMiddleware>();
+
 app.MapControllers();
+
+// Middleware to access static files in the wwwroot folder
+app.UseStaticFiles();
 
 app.Run();
